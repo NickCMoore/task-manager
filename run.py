4f9ed414b594e92cbec86f23830d1740584e1810
NickCMoore <moorenicholasc@gmail.com>
2024-04-05 15:03:12 +0100

    Address template inheritance

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import os
 from taskmanager import app
 
-
 if __name__ == "__main__":
+    # Set environment variables
+    os.environ['IP'] = '127.0.0.1'  # Replace with your desired IP address
+    os.environ['PORT'] = '5500'      # Replace with your desired port number
+    os.environ['DEBUG'] = 'True'     # Set to 'True' to enable debug mode, 'False' otherwise
+
+    # Run the Flask application
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        debug=os.environ.get("DEBUG") == 'True'
     )
